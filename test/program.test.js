69ace21209ec9e650ab410aee293bf7103556ba5
Steven Fongue <sfongue@gmail.com>
2018-02-09 09:28:48 +0100

    Exercise 1

--- test/program.test.js
+++ test/program.test.js
@@ -8,5 +8,25 @@ describe('Function transformCheckpoint', function() {
   it('Function transformCheckpoint without parameter should return false', function() {
     expect(transformCheckpoint()).to.be.false;
   });
+  it('Function transformCheckpoint With parameter should return true', function() {
+    var input = {
+      id: 'whataw0nd3rful1d',
+      uuid: 'whataw0nd3rful1d',
+      address: 'unknown',
+      addressType: 'unknown',
+      connectable: true,
+      advertisement: {
+        localName: undefined,
+        txPowerLevel: undefined,
+        manufacturerData: undefined,
+        serviceData: [],
+        serviceUuids: [ 'abcd' ]
+      },
+      rssi: -66,
+      services: null,
+      state: 'outofcontrol'
+    };
+    expect(transformCheckpoint(input)).to.be.true;
+  });
 
 });
